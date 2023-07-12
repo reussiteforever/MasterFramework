22d5ce7f247b9cbb4e35425123888323bff87e5e
reussiteforever <khafo5852@gmail.com>
2023-07-12 16:34:14 +0000

    added mapping for config file to framework config

---
+++ src/main/java/com/tmb/config/ConfigFactory.java
@@ -0,0 +1,11 @@
+package com.tmb.config;
+
+import org.aeonbits.owner.ConfigCache;
+
+public final class ConfigFactory {
+    private ConfigFactory(){}
+
+    public static FrameworkConfig getConfig(){
+        return ConfigCache.getOrCreate(FrameworkConfig.class);
+    }
+}
